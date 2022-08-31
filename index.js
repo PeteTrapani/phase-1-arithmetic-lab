b1bcbe6587f81ef5933fac155fb7dee45e8cb675
Pete Trapani <pete.trapani91@gmail.com>
2022-08-31 11:54:52 -0500

    commit tree 311059256e5db2f397845abd302517cbdda65405

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 62
+const num2 = 1
 
-// Write your code here
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 12
+const num4 = 3
+
+const mod = num3 / num4
+
+const max = Math.max(5,10,15,20)
